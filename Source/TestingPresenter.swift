658d31188cd22e0f3ecc13680554a823f4abadc3
Felipe de Sousa Silva <felipe.desousasilva@netlight.com>
2018-07-17 16:40:38 +0200

    uncommenting methods

--- Source/TestingPresenter.swift
+++ Source/TestingPresenter.swift
@@ -10,14 +10,14 @@ import Foundation
 
 // sourcery: MockableProtocol
 protocol TestingPresenter {
-//  var myVariableSet: (() -> String) { get }
-//  var niceVariable2: Int? { get set }
+  var myVariable: (() -> String) { get }
+  var myVariableWithSet: Int? { get set }
 
-//  func emptyMethod()
-//  func methodWithReturn() -> String
-//  func methodWithThrow() throws -> String
+  func emptyMethod()
+  func methodWithReturn() -> String
+  func methodWithThrow() throws -> String
   func methodWithSingleParam(crvsh: String) -> Bool
-//  func methodWithMultipleParams(name: String, lastName: String)
-//  func methodThrowWithParamsAndReturn(param1: String, param2: String) throws -> Bool
-//  func methodWithScape(completion: @escaping (Bool, Error?) -> Void)
+  func methodWithMultipleParams(name: String, lastName: String)
+  func methodThrowWithParamsAndReturn(param1: String, param2: String) throws -> Bool
+  func methodWithScape(completion: @escaping (Bool, Error?) -> Void)
 }
--- Tests/AutoMockable.generated.swift
+++ Tests/AutoMockable.generated.swift
@@ -8,6 +8,54 @@
 // ======================== TestingPresenter ========================
 
 class TestingPresenterMock: TestingPresenter {
+  var myVariableReturnValue: (() -> String)!
+  var myVariable: (() -> String) {
+    get { return myVariableReturnValue }
+    set(value) { myVariableReturnValue = value }
+  }
+
+  var myVariableWithSet: Int?
+
+  // MARK: - emptyMethod
+
+  var emptyMethodCallsCount = 0
+  var emptyMethodWasCalled: Bool {
+    return emptyMethodCallsCount > 0
+  }
+
+  func emptyMethod() {
+    emptyMethodCallsCount += 1
+  }
+
+  // MARK: - methodWithReturn
+
+  var methodWithReturnReturnValue: String!
+  var methodWithReturnCallsCount = 0
+  var methodWithReturnWasCalled: Bool {
+    return methodWithReturnCallsCount > 0
+  }
+
+  func methodWithReturn() -> String {
+    methodWithReturnCallsCount += 1
+    return methodWithReturnReturnValue
+  }
+
+  // MARK: - methodWithThrow
+
+  var methodWithThrowThrowableError: Error?
+  var methodWithThrowReturnValue: String!
+  var methodWithThrowCallsCount = 0
+  var methodWithThrowWasCalled: Bool {
+    return methodWithThrowCallsCount > 0
+  }
+
+  func methodWithThrow() throws -> String {
+    if let error = methodWithThrowThrowableError {
+      throw error
+    }
+    methodWithThrowCallsCount += 1
+    return methodWithThrowReturnValue
+  }
 
   // MARK: - methodWithSingleParam
 
@@ -22,5 +70,50 @@ class TestingPresenterMock: TestingPresenter {
     methodWithSingleParamCrvshCallsCount += 1
     methodWithSingleParamCrvshReceivedCrvsh = crvsh
     return methodWithSingleParamCrvshReturnValue
+  }
+
+  // MARK: - methodWithMultipleParams
+
+  var methodWithMultipleParamsNameLastNameReceivedArguments: [(name: String, lastName: String)] = []
+  var methodWithMultipleParamsNameLastNameCallsCount = 0
+  var methodWithMultipleParamsNameLastNameWasCalled: Bool {
+    return methodWithMultipleParamsNameLastNameCallsCount > 0
+  }
+
+  func methodWithMultipleParams(name: String, lastName: String) {
+    methodWithMultipleParamsNameLastNameCallsCount += 1
+    methodWithMultipleParamsNameLastNameReceivedArguments.append((name: name, lastName: lastName))
+  }
+
+  // MARK: - methodThrowWithParamsAndReturn
+
+  var methodThrowWithParamsAndReturnParam1Param2ThrowableError: Error?
+  var methodThrowWithParamsAndReturnParam1Param2ReceivedArguments: [(param1: String, param2: String)] = []
+  var methodThrowWithParamsAndReturnParam1Param2ReturnValue: Bool!
+  var methodThrowWithParamsAndReturnParam1Param2CallsCount = 0
+  var methodThrowWithParamsAndReturnParam1Param2WasCalled: Bool {
+    return methodThrowWithParamsAndReturnParam1Param2CallsCount > 0
+  }
+
+  func methodThrowWithParamsAndReturn(param1: String, param2: String) throws -> Bool {
+    if let error = methodThrowWithParamsAndReturnParam1Param2ThrowableError {
+      throw error
+    }
+    methodThrowWithParamsAndReturnParam1Param2CallsCount += 1
+    methodThrowWithParamsAndReturnParam1Param2ReceivedArguments.append((param1: param1, param2: param2))
+    return methodThrowWithParamsAndReturnParam1Param2ReturnValue
+  }
+
+  // MARK: - methodWithScape
+
+  var methodWithScapeCompletionReceivedCompletion: ((Bool, Error?) -> Void)?
+  var methodWithScapeCompletionCallsCount = 0
+  var methodWithScapeCompletionWasCalled: Bool {
+    return methodWithScapeCompletionCallsCount > 0
+  }
+
+  func methodWithScape(completion: @escaping (Bool, Error?) -> Void) {
+    methodWithScapeCompletionCallsCount += 1
+    methodWithScapeCompletionReceivedCompletion = completion
   }
 }
